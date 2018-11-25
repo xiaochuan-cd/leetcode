dae2d13fb7d88351fa8917bc46f751f444e36130
xiancuan-cd <15008216625@163.com>
2018-11-25 09:05:53 +0800

    threeSumClosest.py

--- threeSumClosest.py
+++ threeSumClosest.py
@@ -23,12 +23,6 @@ class Solution:
 
                     if s == target:
                         return s
-                        l += 1
-                        r -= 1
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1
-                        while r > l and nums[r] == nums[r+1]:
-                            r -= 1
                     elif s > target:
                         r -= 1
                     else:
