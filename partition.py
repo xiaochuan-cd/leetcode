55c2171081f38f52a600d20f0e5e6958220b7a72
xiancuan-cd <15008216625@163.com>
2018-12-26 22:45:54 +0800

    partition.py

---
+++ partition.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+
+        l, r = ListNode(0), ListNode(0)
+        L, R = l, r
+        while head:
+            if head.val < x:
+                l.next = head
+                l = l.next
+            else:
+                r.next = head
+                r = r.next
+            head = head.next
+        r.next = None
+        l.next = R.next
+        return L.next
+
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(4)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(2)
+    head.next.next.next.next = ListNode(5)
+    head.next.next.next.next.next = ListNode(2)
+    Solution().partition(head, 3)
