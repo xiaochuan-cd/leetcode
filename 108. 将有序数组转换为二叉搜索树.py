8da7fb9c1a8344f5f258936c8a7d6cd25d3b3393
xiancuan-cd <15008216625@163.com>
2019-04-16 21:34:23 +0800

    108. 将有序数组转换为二叉搜索树.py

---
+++ 108. 将有序数组转换为二叉搜索树.py
@@ -0,0 +1,13 @@
+class Solution(object):
+
+    def sortedArrayToBST(self, nums):
+
+        if len(nums) == 0:
+            return None
+
+        mid = len(nums) // 2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1:])
+
+        return node
